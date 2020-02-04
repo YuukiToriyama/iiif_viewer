fe9361d3e0c9a55e4ff33ba98ff6bb4d1260b292
ytoriyama <ytoriyama@torisfactory.com>
2020-02-04 20:32:34 +0900

    modify app.js

--- app.js
+++ app.js
@@ -1,45 +1,42 @@
 /* app.js */
 
-function getManifest(uri) {
-    var manifest = null;
-    var httpObj = new XMLHttpRequest();
-    httpObj.open("GET", uri, true);
-    httpObj.onload = function () {
-        manifest = JSON.parse(this.responseText);
-        showMetadata(manifest);
-        showImages(manifest);
-    }
-    httpObj.send(null);
+async function getManifest(uri) {
+	const response = await fetch(uri).catch(err => {alert(err); return err});
+	const manifest = await response.json();
+	showMetadata(manifest);
+	showImages(manifest);
 }
 
 function showImages(data) {
-    var container = document.getElementById("container");
+	let container = document.getElementById("container");
 
-    var html = "";
-    data.sequences[0].canvases.forEach(elm => {
-        html = html + `<div class="page"><a href="javascript:changeImage('` + elm.images[0].resource['@id'] + `');"><img src="${elm.thumbnail['@id']}" alt="${elm.label}"></a><small>${elm.label}</small></div>`;
-    });
+	let html = "";
+	data.sequences[0].canvases.forEach(elm => {
+		html = html + `<div class="page"><a href="javascript:changeImage('` + elm.images[0].resource['@id'] + `');"><img src="${elm.thumbnail['@id']}" alt="${elm.label}"></a><small>${elm.label}</small></div>`;
+	});
 
-    container.innerHTML = html;
+	container.innerHTML = html;
 }
 
 function showMetadata(data) {
-    var sideBar = document.getElementById("side");
-    var html = `<h2>${data.label}</h2>`;
-    var tmp = "";
-    data.metadata.forEach(elm => {
-        tmp = tmp + `<tr><td>${elm.label}</td><td>${elm.value}</td></tr>`;
-    });
-    html = html + `<table>${tmp}</table>`;
-    html = html + `<img src="${data.logo}" alt="${data.attribution}">`;
+	let sideBar = document.getElementById("side");
+	let html = `<h2>${data.label}</h2>`;
+	let tmp = "";
+	data.metadata.forEach(elm => {
+		tmp = tmp + `<tr><td>${elm.label}</td><td>${elm.value}</td></tr>`;
+	});
+	html = html + `<table>${tmp}</table>`;
+	html = html + `<img src="${data.logo}" alt="${data.attribution}">`;
 
-    sideBar.innerHTML = html;
+	sideBar.innerHTML = html;
 }
 
 function changeImage(uri) {
-    var imageArea = document.getElementById("image");
+	let imageArea = document.getElementById("image");
+	imageArea.childNodes.forEach(elem => {elem.remove()});
+	let image = document.createElement("img");
+	image.setAttribute("src", uri);
+	image.setAttribute("alt", "main_image");
 
-    var html = `<img src="${uri}" alt="main image">`;
-
-    imageArea.innerHTML = html;
+	imageArea.appendChild(image);
 }
